711a863f95071ecfb89003aa4dc2c253aa3561ff
SoRa0000 <d.bayasgalan1230@gmail.com>
2023-07-31 17:00:53 +0800

    feature/search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
-import IBook from "@library/types/IBook";
-import Image from "next/image";
-import Header from "@library/layout/Header";
-
-=======
->>>>>>> 79fd57f649714fb14bc18081b2d3bf0b07d7e6e0
 export default function Home() {
     return <div className="bg-slate-100"></div>;
 }
