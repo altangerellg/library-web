71ac230155b1b0b30303b1667b8349743260c93a
SoRa0000 <d.bayasgalan1230@gmail.com>
2023-08-01 11:13:37 +0800

    fixed filter by author and format

--- src/types/IBook.ts
+++ src/types/IBook.ts
@@ -1,7 +1,7 @@
 import IAuthor from "@library/types/IAuthor";
 
 export default interface IBook {
-    author?: string;
+    author?: IAuthor;
     description?: string;
     isbn: string;
     publicationDate: string;
