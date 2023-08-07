26c3be845de09d62cae3c1aac43d50aff6b31cc0
SoRa0000 <d.bayasgalan1230@gmail.com>
2023-08-07 15:36:35 +0800

    changed url

--- src/app/books/[bookId]/layout.tsx
+++ src/app/books/[bookId]/layout.tsx
@@ -7,17 +7,17 @@ interface Props{
     }
 }
 export async function generateMetadata({params: {bookId}}:Props){
-    const res = await axios.get(`http://206.189.145.130/api/book/find/${bookId}`);
+    const res = await axios.get(`${process.env.BACKEND_URL}/api/book/find/${bookId}`);
     const book:IBook = res.data;
     if(book){
         return{
             title: book.name,
             openGraph: {
-                images:`http://206.189.145.130/public/uploads/${book.coverUrl}`
+                images:`${process.env.BACKEND_URL}/public/uploads/${book.coverUrl}`
               },
             description: book.description?.substring(0,120),
             twitter:{
-                images:`http://206.189.145.130/public/uploads/${book.coverUrl}`
+                images:`${process.env.BACKEND_URL}/public/uploads/${book.coverUrl}`
             }
         }
     }
