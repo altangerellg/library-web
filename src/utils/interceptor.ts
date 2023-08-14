5cb6edbf709560c7179f8ed4be59bf3cddca23e4
BiGoyy <obilguun498@gmail.com>
2023-08-14 15:56:08 +0800

    vercel deployment fixed

--- src/utils/interceptor.ts
+++ src/utils/interceptor.ts
@@ -1,6 +1,6 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
-export default axios.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+export default axios.interceptors.request.use((config: any) => {
     const tmp = localStorage.getItem("data");
     if (tmp) {
         const user = JSON.parse(tmp);
